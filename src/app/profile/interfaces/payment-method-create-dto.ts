a105aead5188e8c1a6ff5e9dbddaa4f7c82de0ad
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-05-01 12:36:55 -0700

    Remove unnecesary parametter

--- src/app/profile/interfaces/payment-method-create-dto.ts
+++ src/app/profile/interfaces/payment-method-create-dto.ts
@@ -1,10 +1,8 @@
 export interface PaymentMethodCreateDto {
-    UserId: string;
     CardNumber: string;
     Brand: string;
     ExpMonth: number;
     ExpYear: number;
     CardHolderName: string;
     IsDefault: boolean;
-
 }
