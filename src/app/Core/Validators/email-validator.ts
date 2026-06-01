8e2ad77e2f834e90f872d0076f48e368afacf505
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-06-01 14:33:04 -0700

    Fixed regex expresion

--- src/app/Core/Validators/email-validator.ts
+++ src/app/Core/Validators/email-validator.ts
@@ -2,5 +2,5 @@ import { Validators } from "@angular/forms";
 
 
 export const EmailValidator = Validators.pattern(
-  /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 );
