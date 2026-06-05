861df7dcc6b2bc23b60aeb588cbd2c44ffc5b12f
Jorge Gerardo <jorgegerardo0105@gmail.com>
2026-06-05 12:57:33 -0700

    Migrated ng19 to ng20

--- src/app/profile/components/no-address-registered/no-address-registered.component.ts
+++ src/app/profile/components/no-address-registered/no-address-registered.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-no-address-registered',
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink],
   template: `
   <section class="d-flex flex-column align-items-center justify-content-center text-center py-5">
     <i class="bi bi-geo-alt display-4 text-muted mb-3"></i>
